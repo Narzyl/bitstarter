8028956795ded3cfee7a33ee97def2a569800448
Ubuntu <ubuntu@ip-172-31-28-108.us-west-2.compute.internal>
2013-07-06 17:31:04 +0000

    Error fix

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var indexContent = fs.readFileSync('index.html', { encoding: 'utf8' });
+  var indexContent = fs.readFileSync('index.html', { encoding: 'utf-8' });
   response.send(indexContent);
 });
 
