b5c508dd2d4b946057ac5e4286c7117df076a9a1
Ubuntu <ubuntu@ip-172-31-40-215.us-west-2.compute.internal>
2013-07-08 21:46:03 +0000

    final version of checks.json and grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +45,12 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(html, checksfile, htmlIsFile) {
+    if (htmlIsFile == true) {
+        $ = cheerioHtmlFile(html);
+    } else {
+        $ = cheerio.load(html);
+    }
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +66,35 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(checkFile) {
+    var checkUrlFile = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + response.message);
+        } else {
+            processChecking(result, checkFile, false);
+        }
+    };
+    return checkUrlFile;
+};
+
+var processChecking = function(html, checksFile, htmlIsFile) {
+  var checkJson = checkHtmlFile(html, checksFile, htmlIsFile);
+  var outJson = JSON.stringify(checkJson, null, 4);
+  console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'URL of the file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+      var callback = buildfn(program.checks);
+      rest.get(program.url).on('complete', callback);
+    } else {
+      processChecking(program.file, program.checks, true);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
